1ff4300c74bc6ca87689adbcb7a34fbc06915a77
W0198745JC <w0198745@campus.nscc.ca>
2024-10-11 16:05:20 -0300

    started pseudocode

--- A2-P1-Landscape.py
+++ A2-P1-Landscape.py
@@ -1,11 +1,13 @@
 #Program 1 – Landscape Calculator
 #A landscaping company needs a program that computes the price of landscaping for a new housing development. 
 
-#Student #:     
-#Student Name:  
+#Student #: W0198745   
+#Student Name:  Jenille Cheney
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+#base pay variable    
+#inputs for  Address, property Depth, Property Width, Type of Grass,number of trees
 
 
 
