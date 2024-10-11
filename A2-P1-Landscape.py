47b5769d798cc1b3b3da2abecebdec03b6c59004
W0198745JC <w0198745@campus.nscc.ca>
2024-10-11 17:19:37 -0300

    tests worked

--- A2-P1-Landscape.py
+++ A2-P1-Landscape.py
@@ -10,6 +10,8 @@ def main():
     baseCharge= 1000
     treeCost= 100
 #inputs for  Address, property Depth, Property Width, Type of Grass,number of trees
+    house=input("Enter house number: ")
+    print("")
     Depth= int(input("Enter property depth (feet) :"))
     print("")
     Width= int(input("Enter property width (feet) :"))
@@ -20,17 +22,24 @@ def main():
 
 # determine square footage
     totalSurface= Depth*Width
+    treeCharge=treeCost*trees
 #if square footage above 5000 add $500 / # if elif grass types 
-    if totalSurface >= 5000:
-        extraCharge= baseCharge+totalSurface
-        if grassType == "fescue":
-            grassCharge= totalSurface*0.05
-        elif grassType == "bentgrass":
-            grassCharge= totalSurface*0.02
-        else:
-            grassCharge=totalSurface*0.01
+    if totalSurface > 5000:                                      #orginally had >= but realized it only needed to be greater
+        extraCharge= 500
     else:
         extraCharge= 0
+    # no longer nested
+    if grassType == "fescue":
+            grassCharge= totalSurface*0.05
+    elif grassType == "bentgrass":
+            grassCharge= totalSurface*0.02
+    else:
+            grassCharge= totalSurface*0.01
+        # realized that by originally nesting this if when the totalSurface was less than 5000 it didn't work so i Seperated the if statements found this out on my last set of testing numbers
+# total cost equation
+    totalCost=grassCharge+extraCharge+treeCharge+baseCharge
+ #Final Print Statement
+    print("Total cost for house {0} is: $ {1:.2f}".format(house,totalCost))
 
 
 
