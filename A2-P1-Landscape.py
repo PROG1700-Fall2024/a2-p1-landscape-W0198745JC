3d1260e472b2f4c2bc490867799afe2d1b75e0e0
W0198745JC <w0198745@campus.nscc.ca>
2024-10-11 16:35:32 -0300

    started coding if and elif

--- A2-P1-Landscape.py
+++ A2-P1-Landscape.py
@@ -6,8 +6,32 @@
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
-#base pay variable    
+#base pay variable   / Tree cost  
+    baseCharge= 1000
+    treeCost= 100
 #inputs for  Address, property Depth, Property Width, Type of Grass,number of trees
+    Depth= int(input("Enter property depth (feet) :"))
+    print("")
+    Width= int(input("Enter property width (feet) :"))
+    print("")
+    grassType=input("Enter type of grass (fescue, bentgrass, campus) :").lower()
+    print("")
+    trees= int(input("Enter the number of trees :"))
+
+# determine square footage
+    totalSurface= Depth*Width
+#if square footage above 5000 add $500 / # if elif grass types 
+    if totalSurface >= 5000:
+        extraCharge= baseCharge+totalSurface
+        if grassType == "fescue":
+            grassCharge= totalSurface*0.05
+        elif grassType == "bentgrass":
+            grassCharge= totalSurface*0.02
+        else:
+            grassCharge=totalSurface*0.01
+    else:
+        extraCharge= 0
+
 
 
 
